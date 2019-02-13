6621f653fee2708c2832a9619d4b8ce4e6757d9d
lxhan <boshetoonmai@gmail.com>
2019-02-13 19:00:11 +0900

    Test

---
+++ content/projects/instabot.md
@@ -0,0 +1,6 @@
+---
+title: "Instabot"
+date: 2019-02-13T18:59:44+09:00
+draft: true
+---
+
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t sam 
 
 cd public
 
