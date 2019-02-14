142472e7de176d832335bed00580ce1f5d94725d
lxhan <boshetoonmai@gmail.com>
2019-02-14 13:59:27 +0900

    Add color to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Commit message:"
+# Color variables
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+NOCOLOR="\033[0m"
+
+echo -e "${YELLOW}Commit message: ${NOCOLOR}"
 read MSG
 
 # Check if message is not empty
 if [ -z "$MSG" ]; then
-    echo "Commit message is required!"
+    echo -e "${RED}Commit message is required!"
     return
 fi
 
@@ -13,24 +19,24 @@ hugo && cd ..
 
 # Check if there is public folder
 if [ ! -d "public" ]; then
-    echo "There is no public folder, trying to recreate..."
+    echo -e "${YELLOW}There is no public folder, trying to recreate..."
     mkdir public && cd public
     git clone git@github.com:lxhan/lxhan.github.io.git .
 elif [ -d "public" ]; then
     cd public && git pull
-    echo "Changed directory to $(pwd)"
+    echo -e "${YELLOW}Changed directory to ${NOCOLOR} $(pwd)"
     git add . && git commit -m "$MSG" && git push
 else
-    echo "Something wrong with the tree structure"
+    echo -e "${RED}Something wrong with the tree structure"
     return
 fi
 
 cd ../site
-echo "Changed directory to $(pwd)"
+echo -e "${YELLOW}Changed directory to ${NOCOLOR} $(pwd)"
 
 # Push to hugo site
 git add .
 git commit -m "$MSG"
 git push
 
-echo "Deployment finished"
+echo -e "${GREEN}Deployment finished"
